983d149a207270bf9a2a849ee7ee0ae910335f16
alameen <alameenraji31@gmail.com>
2020-08-26 02:13:02 +0100

    debugging

--- articles/admin.py
+++ articles/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ArticleModel, ImageModel, TextModel
+from .models import ArticleModel, ImageModel, TextModel, SearchQuery
 
 
 class ArticleImageAdmin(admin.StackedInline):
@@ -24,3 +24,5 @@ admin.register(ImageModel,ArticleImageAdmin)
 
 
 admin.register(TextModel,ArticleTextAdmin)
+
+admin.site.register(SearchQuery)
